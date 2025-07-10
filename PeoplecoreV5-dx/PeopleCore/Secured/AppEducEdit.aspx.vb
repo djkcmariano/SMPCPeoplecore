72a0dc562069f743b93ae0aff960fba66d1fb4f9
famandafe-peoplecore <144511054+famandafe-peoplecore@users.noreply.github.com>
2025-07-10 14:30:21 +0800

    Add restsharp

--- PeoplecoreV5-dx/PeopleCore/Secured/AppEducEdit.aspx.vb
+++ PeoplecoreV5-dx/PeopleCore/Secured/AppEducEdit.aspx.vb
@@ -1,4 +1,7 @@
 ﻿Imports clsLib
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports RestSharp
 Imports System.Data
 
 Partial Class Secured_AppEducEdit
@@ -9,8 +12,8 @@ Partial Class Secured_AppEducEdit
 
     Protected Sub PopulateGrid()
         Try
-            Dim dt As DataTable            
-            dt = SQLHelper.ExecuteDataTable("EApplicantEduc_Web", UserNo, TransNo)                        
+            Dim dt As DataTable
+            dt = SQLHelper.ExecuteDataTable("EApplicantEduc_Web", UserNo, TransNo)
             grdMain.DataSource = dt
             grdMain.DataBind()
         Catch ex As Exception
@@ -22,7 +25,7 @@ Partial Class Secured_AppEducEdit
         Try
             Dim dt As DataTable
             dt = SQLHelper.ExecuteDataTable("EApplicantEduc_WebOne", UserNo, id)
-            For Each row As DataRow In dt.Rows                
+            For Each row As DataRow In dt.Rows
                 Generic.PopulateData(Me, "Panel1", dt)
                 ViewState("CourseNo") = Generic.ToInt(row("CourseNo"))
                 ViewState("SchoolDetiNo") = Generic.ToInt(row("SchoolDetiNo"))
@@ -62,12 +65,27 @@ Partial Class Secured_AppEducEdit
             Generic.ClearControls(Me, "Panel1")
             PopulateControls()
             ModalPopupExtender1.Show()
+            'Dim json As String = "[{""ApplicantNo"":1,""EmployeeNo"":0,""CityNo"":0,""SchoolNo"":97,""CourseNo"":412,""EducLevelNo"":3,""FromDate"":"""",""ToDate"":"""",""FromMonth"":6,""FromDay"":1,""FromYear"":1997,""ToMonth"":3,""ToDay"":31,""ToYear"":2001,""UnitEarned"":0.0,""IsGraduated"":0,""HonorNo"":0,""YearGrad"":0,""OtherCourse"":"""",""EncodeNo"":1,""EncodeDate"":""2025-07-03T19:13:45.303"",""HonorDate"":null,""NonHonor"":"""",""NonHonorDate"":null,""FieldOfStudy"":"""",""Address"":"""",""IsUpdated"":0,""IsDeleted"":0,""IsUsed"":0,""PayLocNo"":0,""OtherSchool"":"""",""OtherEducLevel"":"""",""IsHighest"":false,""IsOtherSchool"":false,""IsOtherCourse"":false,""FieldOfStudyNo"":0,""Scholarship"":"""",""SchoolDetiNo"":0,""IsPresent"":0,""IsCareer"":1,""CareerDate"":""2025-07-03T19:13:45.303"",""IsApplyToAll"":0,""Remarks"":"""",""IsFixed"":0,""EffectiveDate"":null,""DocRef"":""""}]"
+            'Dim factory As New RestSharpClientFactory()
+            'Dim client As RestClient = factory.GetClient()
+
+            'Dim request As New RestRequest("api/push/onejsondata", Method.Get)
+            'request.AddJsonBody(New With {
+            '        .totalRows = 1,
+            '        .hasMore = False,
+            '        .content = json,
+            '        .tableName = "EApplicantEduc"
+            '    })
+            'Dim response As RestResponse = client.Execute(request)
+            'If response.IsSuccessful Then
+            '    Dim result As String = response.Content
+            'End If
         Else
             MessageBox.Warning(MessageTemplate.DeniedAdd, Me)
         End If
     End Sub
 
-    Protected Sub lnkSave_Click(sender As Object, e As EventArgs)      
+    Protected Sub lnkSave_Click(sender As Object, e As EventArgs)
         Dim RetVal As Boolean = False
         Dim ApplicantEducNo As Integer = Generic.ToInt(Me.txtApplicantEducNo.Text)
         Dim EducLevelNo As Integer = Generic.ToInt(Me.cboEducLevelNo.SelectedValue)
@@ -92,7 +110,7 @@ Partial Class Secured_AppEducEdit
 
         Dim invalid As Boolean = True, messagedialog As String = "", alerttype As String = ""
         Dim dtx As New DataTable, dt As New DataTable, error_num As Integer = 0, error_message As String = ""
-        dtx = SQLHelper.ExecuteDataTable("EApplicantEduc_WebValidate", UserNo, ApplicantEducNo, TransNo, EducLevelNo, txtFieldOfStudy.Text, CourseNo, SchoolNo, txtSchoolAddress.Text, FromMonth, FromYear, ToMonth, ToYear, YearGrad, UnitEarned, HonorNo, IsOtherSchool, OtherSchool, IsOtherCourse, OtherCourse, FromDay, Today, IsHighest)
+        dtx = SQLHelper.ExecuteDataTable("EApplicantEduc_WebValidate", UserNo, ApplicantEducNo, TransNo, EducLevelNo, txtFieldOfStudy.Text, CourseNo, SchoolNo, txtSchoolAddress.Text, FromMonth, FromYear, ToMonth, ToYear, YearGrad, UnitEarned, HonorNo, IsOtherSchool, OtherSchool, IsOtherCourse, OtherCourse, FromDay, ToDay, IsHighest)
 
         For Each rowx As DataRow In dtx.Rows
             invalid = Generic.ToBol(rowx("tProceed"))
@@ -106,16 +124,50 @@ Partial Class Secured_AppEducEdit
             Exit Sub
         End If
 
-        If SQLHelper.ExecuteNonQuery("EApplicantEduc_WebSave", TransNo, ApplicantEducNo, TransNo, EducLevelNo, txtFieldOfStudy.Text, CourseNo, SchoolNo, txtSchoolAddress.Text, FromMonth, FromYear, ToMonth, ToYear, YearGrad, UnitEarned, HonorNo, IsOtherSchool, OtherSchool, IsOtherCourse, OtherCourse, FromDay, ToDay, IsHighest, FieldOfStudyNo, txtScholarship.Text, Generic.ToInt(cboSchoolDetiNo.SelectedValue)) > 0 Then
-            RetVal = True
-        Else
-            RetVal = False
-        End If
+        'If SQLHelper.ExecuteNonQuery("EApplicantEduc_WebSave", TransNo, ApplicantEducNo, TransNo, EducLevelNo, txtFieldOfStudy.Text, CourseNo, SchoolNo, txtSchoolAddress.Text, FromMonth, FromYear, ToMonth, ToYear, YearGrad, UnitEarned, HonorNo, IsOtherSchool, OtherSchool, IsOtherCourse, OtherCourse, FromDay, ToDay, IsHighest, FieldOfStudyNo, txtScholarship.Text, Generic.ToInt(cboSchoolDetiNo.SelectedValue)) > 0 Then
+        '    RetVal = True
+        'Else
+        '    RetVal = False
+        'End If
+
+        'Saving through web api
+        Dim dt1 As DataTable = SQLHelper.ExecuteDataTable("EApplicantEduc_WebSave", TransNo, ApplicantEducNo, TransNo, EducLevelNo, txtFieldOfStudy.Text, CourseNo, SchoolNo, txtSchoolAddress.Text, FromMonth, FromYear, ToMonth, ToYear, YearGrad, UnitEarned, HonorNo, IsOtherSchool, OtherSchool, IsOtherCourse, OtherCourse, FromDay, ToDay, IsHighest, FieldOfStudyNo, txtScholarship.Text, Generic.ToInt(cboSchoolDetiNo.SelectedValue))
+        Dim json As String = JsonConvert.SerializeObject(dt1)
+        Try
+            Dim factory As New RestSharpClientFactory()
+            Dim client As RestClient = factory.GetClient()
+
+            Dim request As New RestRequest("api/push/onejsondata", Method.Post)
+            request.AddBody(New With {
+                .totalRows = 1,
+                    .hasMore = False,
+                    .content = json,
+                    .tableName = "EApplicantEduc"
+                })
+
+            Dim response As RestResponse = client.Execute(request)
+            If response.IsSuccessful Then
+                Dim jsonData = JsonConvert.DeserializeObject(Of APIStatus)(response.Content)
+                Dim arr As JArray = JArray.Parse(json)
+                arr(0)("ApplicantEducNo") = jsonData.Id
+                json = arr.ToString(Newtonsoft.Json.Formatting.None)
+                SQLHelper.ExecuteNonQuery("EJSONMain_WebSave", json, "EApplicantEduc")
+                RetVal = True
+            Else
+                RetVal = False
+                error_message = "Unable to save record in career portal server."
+            End If
+        Catch ex As Exception
+            error_message = ex.Message
+        End Try
 
         If RetVal = True Then
             PopulateGrid()
             MessageBox.Success(MessageTemplate.SuccessSave, Me)
+        Else
+            MessageBox.Warning(error_message, Me)
         End If
+
     End Sub
 
     Protected Sub lnkEdit_Click(sender As Object, e As EventArgs)
@@ -394,7 +446,6 @@ Partial Class Secured_AppEducEdit
     End Sub
 
     Private Sub PopulateSchoolCampus()
-
         Dim dt As DataTable
         dt = SQLHelper.ExecuteDataTable("ESchoolDeti_WebLookup", UserNo, Generic.ToInt(cboSchoolNo.SelectedValue))
         If Generic.ToInt(dt.Rows.Count) > 1 Then
@@ -416,8 +467,6 @@ Partial Class Secured_AppEducEdit
             cboSchoolDetiNo.SelectedValue = ""
             cboSchoolDetiNo.CssClass = "form-control"
         End If
-
-
     End Sub
 
 End Class
